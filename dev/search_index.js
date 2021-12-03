5efdf5be8d1edd0079daf329f22889930d07cb20
Documenter.jl <documenter@juliadocs.github.io>
2021-12-03 22:28:06 +0000

    build based on 0519069

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BlockHankelMatrices","category":"page"},{"location":"#BlockHankelMatrices","page":"Home","title":"BlockHankelMatrices","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BlockHankelMatrices.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BlockHankelMatrices]","category":"page"}]
+}
