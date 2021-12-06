29ae0bdbcdcd4174377ac85787dfffdea50aa972
Zdeněk Hurák <hurak@fel.cvut.cz>
2021-12-06 18:40:06 +0100

    Just some random addition, still not functional.

--- src/typedef.jl
+++ src/typedef.jl
@@ -2,8 +2,8 @@ abstract type AbstractBlockHankel{T<:Number} <: AbstractMatrix{T} end
 
 struct BlockHankel{T<:Number} <: AbstractBlockHankel{T}
     w::Vector{Vector{T}}
-    N = length(w)                                                       # The length of the discrete-time vector signal
-    m = length(w[1])                                                    # The dimension of the (discrete-time )signal
+    N = length(w)                                                                   # The length of the discrete-time vector signal
+    m = length(w[1])                                                                # The dimension of the (discrete-time) signal
     function BlockHankel{T}(w::Vector{Vector{T}},L::S) where {T<:Number,S<:Int}
         if first(vc) != first(vr)
             error("First element of the vectors must be the same")
@@ -12,3 +12,15 @@ struct BlockHankel{T<:Number} <: AbstractBlockHankel{T}
     end
 end
 
+function Matrix(A::AbstractBlockHankel{T}) where T
+    m, n = size(A)
+    Af = Matrix{T}(undef, m, n)
+    for j = 1:n
+        for i = 1:m
+            Af[i,j] = A[i,j]
+        end
+    end
+    return Af
+end
+
+convert(::Type{Matrix}, A::AbstractBlockHankel) = Matrix(A)
